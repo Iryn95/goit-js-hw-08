1f3eff4ea85e4789ce415d6561e2503ac8432c93
Iryna Konchits <uncl95@gmail.com>
2022-08-17 13:10:52 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";// Add imports above this line
 
+// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+
+    const addHtml = galleryItems.map((item) =>
+    `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    alt="${item.description}"/>
+    </a>`
+).join("");
+
+galleryRef.insertAdjacentHTML('beforeend', addHtml)
+    let modal = new SimpleLightbox('.gallery__item', {captionsData:'alt', captionDelay:250});
 console.log(galleryItems);
+
+
